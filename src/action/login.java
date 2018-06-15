27879d918d804a9d714c1d4bb3e00749c38cc5b5
LIN ZHENDAN <zhendan.lin.2016@smu.edu.sg>
2018-06-15 14:19:27 +0800

    initial commit

---
+++ src/action/login.java
@@ -0,0 +1,33 @@
+package action;
+
+public class login {
+
+	private String name,password;  
+	  
+	//getters and setters  
+	  
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String execute(){  
+		if(password.equals("admin")){  
+		    return "success";  
+		}  
+		else{  
+		    return "error";  
+		}  
+	}  
+}  
