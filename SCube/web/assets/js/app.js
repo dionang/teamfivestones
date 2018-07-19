0a10b56cf73d00961a378169d29f8931f87476c5
ANG WEI XUAN, DION _ <dion.ang.2016@smu.edu.sg>
2018-07-19 21:52:28 +0800

    responsive chart
    Signed-off-by: ANG WEI XUAN, DION _ <dion.ang.2016@smu.edu.sg>

--- SCube/web/assets/js/app.js
+++ SCube/web/assets/js/app.js
@@ -1,4 +1,4 @@
-const {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} = Recharts;
+const {ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} = Recharts;
 const data = [
     {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
     {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
@@ -32,18 +32,20 @@ class SimpleLineChart extends React.Component {
             <Rnd default={{
                 x: 0,
                 y: 0,
-                width: 320,
-                height: 200,
-              }}>
-            <LineChart width={320} height={200} data={data} >
-                <XAxis dataKey="name"/>
-                <YAxis/>
-                <CartesianGrid strokeDasharray="3 3"/>
-                <Tooltip/>
-                <Legend />
-                <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-            </LineChart>
+                width: 400,
+                height: 250,
+            }}>
+                <ResponsiveContainer width={"100%"} height="100%">
+                    <LineChart width={"100%"} height={"100%"} data={data} >
+                        <XAxis dataKey="name"/>
+                        <YAxis/>
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <Tooltip/>
+                        <Legend />
+                        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+                        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    </LineChart>
+                </ResponsiveContainer>
             </Rnd>
         );
     }
