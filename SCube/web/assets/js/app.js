1a52001573ca0cea2eb6fab9cc86bd523cc35887
XUE Xinyi <xinyi.xue.2016@smu.edu.sg>
2018-11-16 17:33:47 +0800

    Minor UI Change on the report creation page

--- SCube/web/assets/js/app.js
+++ SCube/web/assets/js/app.js
@@ -686,7 +686,7 @@ class App extends Component {
                             </div> */}
 
 
-                            <div className="col-sm-12 col-xs-12" style={{ paddingTop: 5, backgroundColor: 'white',height:window.innerHeight*0.13, borderBottom:"#003366 5px solid" }}>
+                            <div className="col-sm-12 col-xs-12" style={{backgroundColor: 'white',height:window.innerHeight*0.13, borderBottom:"#003366 5px solid" }}>
                                 <div className="col-md-12 col-xs-12" style={{height:"fit-content", marginBottom:5}}>
                                 <label style={{ fontSize: 15, marginRight: 2, float:"left" }}>Template Name:</label>
                                 <input style={{ fontSize: 15, marginLeft: 10, float:"left" }} value={this.state.templateName} onChange={this.renameTemplate} />
@@ -701,17 +701,17 @@ class App extends Component {
                             
                                 <label> Add Component: </label>
                                 <input type="image" data-toggle="tooltip" title="Add Textbox"
-                                    onClick={this.addTextbox} style={{ marginRight: 0.5, height:window.innerHeight*0.035, marginBottom:"-15px" }} src= "assets/images/text.png"/>
+                                    onClick={this.addTextbox} style={{ marginRight: 0.5, height:window.innerHeight*0.035, marginBottom:"-10px" }} src= "assets/images/text.png"/>
                                 <input type="image" data-toggle="tooltip" title="Add Bar Chart"
-                                    onClick={this.addBarChart} style={{ marginRight: 5, height:window.innerHeight*0.035, marginBottom:"-15px"  }} src= "assets/images/bar chart.png"/>
+                                    onClick={this.addBarChart} style={{ marginRight: 5, height:window.innerHeight*0.035, marginBottom:"-10px"  }} src= "assets/images/bar chart.png"/>
                                 <input type="image"  data-toggle="tooltip"  title="Add Line Chart" 
-                                    onClick={this.addLineChart} style={{ marginRight: 5, height:window.innerHeight*0.035, marginBottom:"-15px"  }} src= "assets/images/line chart.png"/>
+                                    onClick={this.addLineChart} style={{ marginRight: 5, height:window.innerHeight*0.035, marginBottom:"-10px"  }} src= "assets/images/line chart.png"/>
                                 <input type="image"  data-toggle="tooltip"  title="Add Table" 
-                                    onClick={this.addTable} style={{ marginRight: 5, height:window.innerHeight*0.035, marginBottom:"-15px"  }} src= "assets/images/table.png"/>
+                                    onClick={this.addTable} style={{ marginRight: 5, height:window.innerHeight*0.035, marginBottom:"-10px"  }} src= "assets/images/table.png"/>
                                 <input type="image"  data-toggle="tooltip"  title="Add Image" 
-                                    onClick={this.addImage} style={{ marginRight: 5, height:window.innerHeight*0.035, marginBottom:"-15px"  }} src= "assets/images/picture.png"/>
+                                    onClick={this.addImage} style={{ marginRight: 5, height:window.innerHeight*0.035, marginBottom:"-10px"  }} src= "assets/images/picture.png"/>
                                 <input type="image"  data-toggle="tooltip"  title="Add Video" 
-                                    onClick={this.addVideo} style={{ marginRight: 5, height:window.innerHeight*0.035, marginBottom:"-15px"  }} src= "assets/images/video_4880.png"/>
+                                    onClick={this.addVideo} style={{ marginRight: 5, height:window.innerHeight*0.035, marginBottom:"-10px"  }} src= "assets/images/video_4880.png"/>
                             
                                 {/*
                                 <Button data-toggle="tooltip" data-placement="bottom" title="Add Textbox" bsStyle="primary"
